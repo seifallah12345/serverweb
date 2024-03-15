7eb75f6b6a462e8e25804af333064b3b99343b46
oussama chaq <100126869+ocodeur@users.noreply.github.com>
2024-03-14 23:45:52 -0400

    Delete Oussama/validations directory

--- Oussama/validations/validationPayments.js
+++
@@ -1,7 +0,0 @@
-import { body } from "express-validator";
-
-const paymentRules = [
-    body("paymentMethod").notEmpty().withMessage("Le mode de paiement ne peut pas être vide")
-];
-
-export default paymentRules;
--- Oussama/validations/validationPermissions.js
+++
@@ -1,7 +0,0 @@
-import { body } from "express-validator";
-
-const permissionRules = [
-    body("permissionName").notEmpty().withMessage("Le nom de la permission ne peut pas être vide")
-];
-
-export default permissionRules;
